1eea273eacdea08c9781062a3e004519a90c773d
残月 <632342527@qq.com>
2016-11-24 16:14:08 +0800

    Initial Commit

---
+++ 系统自带的人脸识别/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  系统自带的人脸识别
+//
+//  Created by 王奥东 on 16/11/24.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 系统自带的人脸识别/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  系统自带的人脸识别
+//
+//  Created by 王奥东 on 16/11/24.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
